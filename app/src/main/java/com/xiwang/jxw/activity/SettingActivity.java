048282b69ef4ef71360e2bca30ac816607ba2582
lxg776 <262621979@qq.com>
2015-11-20 00:22:29 +0800

    update 1119-1

---
+++ app/src/main/java/com/xiwang/jxw/activity/SettingActivity.java
@@ -0,0 +1,40 @@
+package com.xiwang.jxw.activity;
+
+import com.xiwang.jxw.R;
+import com.xiwang.jxw.base.BaseActivity;
+
+/**
+ * Created by sunshine on 15/11/19.
+ */
+public class SettingActivity extends BaseActivity{
+
+    @Override
+    protected void initActionBar() {
+
+    }
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_setting;
+    }
+
+    @Override
+    protected void findViews() {
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    protected void initGetData() {
+
+    }
+
+    @Override
+    protected void widgetListener() {
+
+    }
+}
--- app/src/main/java/com/xiwang/jxw/fragment/PublishFragment.java
+++ app/src/main/java/com/xiwang/jxw/fragment/PublishFragment.java
@@ -1,6 +1,7 @@
 package com.xiwang.jxw.fragment;
 
 import android.view.View;
+import android.widget.LinearLayout;
 
 import com.xiwang.jxw.R;
 import com.xiwang.jxw.base.BaseFragment;
@@ -13,6 +14,24 @@ import com.xiwang.jxw.base.BaseFragment;
  * @modifier
  */
 public class PublishFragment extends BaseFragment {
+    /** 发帖按钮*/
+    LinearLayout fabu_btn;
+    /** 二手交易*/
+    LinearLayout ershou_btn;
+    /** 招聘信息*/
+    LinearLayout zhaopin_btn;
+    /** 发简历求职*/
+    LinearLayout qiuzhi_btn;
+    /** 房租出租*/
+    LinearLayout chuzu_btn;
+    /** 求租信息*/
+    LinearLayout qiuzu_btn;
+    /** 二手房源*/
+    LinearLayout ershoufang_btn;
+    /** 地皮/商铺信息*/
+    LinearLayout dipi_btn;
+
+
 
 
     @Override
@@ -22,7 +41,14 @@ public class PublishFragment extends BaseFragment {
 
     @Override
     protected void findViews() {
-
+        fabu_btn=findViewById(R.id.fabu_btn);
+        ershou_btn=findViewById(R.id.ershou_btn);
+        zhaopin_btn=findViewById(R.id.zhaopin_btn);
+        qiuzhi_btn=findViewById(R.id.qiuzhi_btn);
+        chuzu_btn=findViewById(R.id.chuzu_btn);
+        qiuzu_btn=findViewById(R.id.qiuzu_btn);
+        ershoufang_btn=findViewById(R.id.ershoufang_btn);
+        dipi_btn=findViewById(R.id.dipi_btn);
     }
 
     @Override
